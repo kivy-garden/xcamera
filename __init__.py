5778eff2b795a4738e219690f8ed0c4df3991069
Antonio Cuni <anto.cuni@gmail.com>
2016-07-05 01:31:26 +0200

    make a nicer shoot button: it's now a circle with a nice camera icon inside

--- __init__.py
+++ __init__.py
@@ -1,9 +1,17 @@
+import os
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
+from kivy.resources import resource_add_path
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+resource_add_path(ROOT)
 
 kv = """
 <XCamera>:
-    icon: 'O'
+    # \ue800 corresponds to the camera icon in the font
+    icon: u"[font=data/xcamera/icons.ttf]\ue800[/font]"
+    icon_color: (0.13, 0.58, 0.95, 0.8)
+    icon_size: dp(50)
 
     Camera:
         id: corecam
@@ -12,14 +20,29 @@ kv = """
         allow_stretch: True
         size_hint: 1, 1
 
+    # Shoot button
     Label:
+        # background circle
+        canvas.before:
+            Color:
+                rgba: root.icon_color
+            Ellipse:
+                pos: self.pos
+                size: self.size
+
+        # foreground camera icon
         markup: True
-        text: "[ref=shutter]%s[/ref]" % root.icon
-        halign: "right"
-        valign: "middle"
-        text_size: self.size
-        font_size: "80dp"
-        on_ref_press: root.shoot()
+        text: u"[ref=x]%s[/ref]" % root.icon
+
+        # size
+        size_hint: None, None
+        size: root.icon_size, root.icon_size
+        font_size: root.icon_size/2
+
+        # position
+        right: root.width - dp(10)
+        center_y: root.center_y
+
 """
 Builder.load_string(kv)
 
