42ad77fdd7a5fec96c8f5cef853185a41efb3156
Antonio Cuni <anto.cuni@gmail.com>
2016-07-05 11:21:52 +0200

    use autofocus and take pictures only when we have the focus

--- android_api.py
+++ android_api.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-from jnius import autoclass, PythonJavaClass, java_method
+from jnius import autoclass, PythonJavaClass, java_method, JavaException
 
 Camera = autoclass('android.hardware.Camera')
 AndroidActivityInfo = autoclass('android.content.pm.ActivityInfo')
@@ -19,9 +19,8 @@ class ShutterCallback(PythonJavaClass):
 class PictureCallback(PythonJavaClass):
     __javainterfaces__ = ('android.hardware.Camera$PictureCallback', )
 
-    def __init__(self, camera, filename):
+    def __init__(self, filename):
         super(PictureCallback, self).__init__()
-        self.camera = camera
         self.filename = filename
 
     @java_method('([BLandroid/hardware/Camera;)V')
@@ -31,20 +30,41 @@ class PictureCallback(PythonJavaClass):
         with open(self.filename, 'wb') as f:
             f.write(s)
         print 'saved to', self.filename
-        self.camera.startPreview()
+        camera.startPreview()
 
 
+class AutoFocusCallback(PythonJavaClass):
+    __javainterfaces__ = ('android.hardware.Camera$AutoFocusCallback', )
+
+    def __init__(self, filename):
+        super(AutoFocusCallback, self).__init__()
+        self.filename = filename
+
+    @java_method('(ZLandroid/hardware/Camera;)V')
+    def onAutoFocus(self, success, camera):
+        if success:
+            shutter_cb = ShutterCallback()
+            picture_cb = PictureCallback(self.filename)
+            camera.takePicture(shutter_cb, None, picture_cb)
+        else:
+            print 'autofocus failed'
 
 
 def take_picture(camera_widget, filename):
-    core_camera = camera_widget._camera
-    camera = core_camera._android_camera # this is a Java android.hardware.Camera
+    # to call the android API, we need access to the underlying
+    # android.hardware.Camera instance. However, there is no official way to
+    # retrieve it from the camera widget, so we need to dig into internal
+    # attributes :-( This works at least on kivy 1.9.1, but it might break any
+    # time soon.
+    camera = camera_widget._camera._android_camera
     params = camera.getParameters()
-    params.setFocusMode("continuous-picture")
+    params.setFocusMode("auto")
     camera.setParameters(params)
-    shutter_cb = ShutterCallback()
-    picture_cb = PictureCallback(camera, filename)
-    camera.takePicture(shutter_cb, None, picture_cb)
+    cb = AutoFocusCallback(filename)
+    try:
+        camera.autoFocus(cb)
+    except JavaException, e:
+        print 'JavaException when calling autofocus', e
 
 
 
