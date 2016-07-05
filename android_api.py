67a1023d0f2229f31a6aae51ce6b43e2ce8a69ef
Antonio Cuni <anto.cuni@gmail.com>
2016-07-05 02:55:09 +0200

    play the shutter sound on android

--- android_api.py
+++ android_api.py
@@ -1,8 +1,20 @@
+from __future__ import absolute_import
 from jnius import autoclass, PythonJavaClass, java_method
 
 Camera = autoclass('android.hardware.Camera')
 AndroidActivityInfo = autoclass('android.content.pm.ActivityInfo')
 AndroidPythonActivity = autoclass('org.renpy.android.PythonActivity')
+
+class ShutterCallback(PythonJavaClass):
+    __javainterfaces__ = ('android.hardware.Camera$ShutterCallback', )
+
+    @java_method('()V')
+    def onShutter(self):
+        # apparently, it is enough to have an empty shutter callback to play
+        # the standard shutter sound. If you pass None instead of shutter_cb
+        # below, the standard sound doesn't play O_o
+        pass
+
 
 class PictureCallback(PythonJavaClass):
     __javainterfaces__ = ('android.hardware.Camera$PictureCallback', )
@@ -23,14 +35,16 @@ class PictureCallback(PythonJavaClass):
 
 
 
+
 def take_picture(camera_widget, filename):
     core_camera = camera_widget._camera
     camera = core_camera._android_camera # this is a Java android.hardware.Camera
     params = camera.getParameters()
     params.setFocusMode("continuous-picture")
     camera.setParameters(params)
-    callback = PictureCallback(camera, filename)
-    camera.takePicture(None, None, callback)
+    shutter_cb = ShutterCallback()
+    picture_cb = PictureCallback(camera, filename)
+    camera.takePicture(shutter_cb, None, picture_cb)
 
 
 
